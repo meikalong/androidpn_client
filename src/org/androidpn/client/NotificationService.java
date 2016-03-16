a1ea1e10dcc031215840d345a51de76f10a08617
meikalong <meikalong>
2016-03-16 17:02:52 +0800

    Signed-off-by: meikalong <meikalong>

--- src/org/androidpn/client/NotificationService.java
+++ src/org/androidpn/client/NotificationService.java
@@ -98,8 +98,7 @@ public class NotificationService extends Service {
 			if (sharedPrefs.contains(Constants.EMULATOR_DEVICE_ID)) {
 				deviceId = sharedPrefs.getString(Constants.EMULATOR_DEVICE_ID, "");
 			} else {
-				deviceId = (new StringBuilder("EMU")).append((new Random(System.currentTimeMillis())).nextLong())
-						.toString();
+				deviceId = (new StringBuilder("EMU")).append((new Random(System.currentTimeMillis())).nextLong()).toString();
 				editor.putString(Constants.EMULATOR_DEVICE_ID, deviceId);
 				editor.commit();
 			}
@@ -247,8 +246,8 @@ public class NotificationService extends Service {
 
 		public Future<?> submit(Runnable task) {
 			Future<?> result = null;
-			if (!notificationService.getExecutorService().isTerminated()
-					&& !notificationService.getExecutorService().isShutdown() && task != null) {
+			if (!notificationService.getExecutorService().isTerminated() && !notificationService.getExecutorService().isShutdown()
+					&& task != null) {
 				result = notificationService.getExecutorService().submit(task);
 			}
 			return result;
